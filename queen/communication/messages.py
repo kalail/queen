c32e9f4e4965ef5cbe5237c16d39d2d2af9cc6d4
Kalail <kalail@ubuntu.(none)>
2013-02-20 08:47:56 -0600

    Add messages file.

---
+++ queen/communication/messages.py
@@ -0,0 +1,17 @@
+
+messages = (
+	{
+		'name': 'Heartbeat',
+		'parameters': {
+		}
+	},
+	{
+		'name': 'HeartbeatResponse',
+		'parameters': {
+			'x': (0, 0),
+			'y': (1, 0),
+			'r': (2, 0),
+			'alive': (3, False),
+		}
+	},
+)

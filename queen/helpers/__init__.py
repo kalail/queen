dac64e4a617713e9170b1f3ce1471f7d3f372613
Kalail <kalail@ubuntu.(none)>
2013-04-05 22:54:14 -0500

    Add importnat msg.

--- queen/helpers/__init__.py
+++ queen/helpers/__init__.py
@@ -2,7 +2,7 @@ import serial
 
 def get_active_drones():
 	port = serial.Serial('/dev/ttyUSB0', 9600)
-	port.write('0;1;0; \n')
+	port.write('0;1;0;GabeIsADick!\n')
 	drones = []
 	
 	msg = port.readline()

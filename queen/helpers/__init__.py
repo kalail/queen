773a18486175e3c9dfeed067ce601c5c5acdcbca
Kalail <kalail@ubuntu.(none)>
2013-04-06 01:12:02 -0500

    Add repeatable functon

--- queen/helpers/__init__.py
+++ queen/helpers/__init__.py
@@ -1,11 +1,15 @@
 import serial
 
 def get_active_drones():
-	port = serial.Serial('/dev/ttyUSB0', 9600)
+	port = serial.Serial('/dev/ttyUSB0', 9600, timeout=2)
 	port.write('0;1;0;GabeIsADick!\n')
 	drones = []
 	
 	msg = port.readline()
+	# Check for timeout
+	if not msg:
+		return None
+	# Extract drone ID
 	drone_id = msg[2]
 	drones.append(drone_id)
 

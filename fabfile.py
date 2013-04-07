0c16ad03a566f3aab1a4afb4c34dccfce01421d3
Kalail <kalail@ubuntu.(none)>
2013-04-07 06:02:54 -0500

    Add deploy fab command.

--- fabfile.py
+++ fabfile.py
@@ -10,9 +10,11 @@ env.user = 'pi'
 def prepare_raspberry_pi():
 	pass
 
-def pull():
+def remote_pull():
 	with cd('virtualenvs/queen/queen'):
 		run('git pull')
 
 def deploy():
-	pass
+	local('git commit -a')
+	local('git push origin')
+	remote_pull()
